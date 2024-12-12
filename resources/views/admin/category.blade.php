3a39df75edc98523183ef19e57b8b7c78b5379b3
HShan99 <shanharsharathnayake@gmail.com>
2024-12-12 18:09:00 +0530

    Create Add category part

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+
+    <style type="text/css">
+        .div_center{
+            text-align: center;
+            padding-top: 40px;
+        }
+        .h2_font{
+            font-size: 30px;
+            padding-bottom: 40px;
+        }
+        .input_color{
+            color: black;
+        }
+
+    </style>
+  </head>
+  <body>
+    <div class="container-scroller">
+
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+
+      <!-- partial:partials/_navbar.html -->
+      @include('admin.header')
+      <!-- partial -->
+
+      <div class="main-panel">
+        <div class="content-wrapper">
+
+            @if (@session()->has('message'))
+            <div class="alert alert-success">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                {{@session()->get('message')}}
+            </div>
+            @endif
+
+            <div class="div_center">
+                <h2 class="h2_font">Add Category</h2>
+
+                <form action="{{url('/add_category')}}" method="POST">
+                    @csrf
+
+                    <input type="text" class="input_color" name="category" placeholder="Write Category Name">
+
+                    <input type="submit" class="btn btn-primary" name="submit" value="Add Category">
+                </form>
+            </div>
+        </div>
+      </div>
+
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-center text-muted d-block text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+              <span class="float-none mt-1 text-center float-sm-right d-block mt-sm-0"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
+</html>
